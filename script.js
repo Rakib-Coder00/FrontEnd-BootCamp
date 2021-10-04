3266e425684d533629ac97e25d1b824c27ab8600
Limitless-Rakib <80609225+Rakib-Coder00@users.noreply.github.com>
2021-10-05 01:15:39 +0600

    intructor & contact section finished

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicmFraWJkZXYiLCJhIjoiY2t1Y3pqOGw4MTVoaTMxbW9oaW5jang0eiJ9.yoSkczzg1sPy__6TAW2zHA";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-71.060982, 42.35725],
+  zoom: 18,
+});
